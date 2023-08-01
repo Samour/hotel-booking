f52a5d332eaeaad5d275d4f0a8fab09a2763a7e3
Samour <aaron.burke.cpp@gmail.com>
2023-08-01 18:39:48 +1000

    Create basic gradle project

---
+++ be/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    group = "me.aburke.hotelbooking"
+    version = "0.0.1-SNAPSHOT"
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
