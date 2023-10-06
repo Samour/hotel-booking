b6dd4023c3a460b96791f8127d3e13fbb33dc7db
Samour <aaron.burke.cpp@gmail.com>
2023-10-07 07:46:10 +1000

    Implement domain logic for hold room
    Still need to add distributed locking

--- be/domain/src/main/kotlin/me/aburke/hotelbooking/scenario/room/HoldRoomScenario.kt
+++ be/domain/src/main/kotlin/me/aburke/hotelbooking/scenario/room/HoldRoomScenario.kt
@@ -1,3 +1,57 @@
 package me.aburke.hotelbooking.scenario.room
 
-class HoldRoomScenario
+import me.aburke.hotelbooking.ports.repository.CreateRoomHoldResult
+import me.aburke.hotelbooking.ports.repository.RoomHoldRepository
+import me.aburke.hotelbooking.ports.scenario.room.HoldRoomDetail
+import me.aburke.hotelbooking.ports.scenario.room.HoldRoomPort
+import me.aburke.hotelbooking.ports.scenario.room.HoldRoomResult
+import java.time.Clock
+import java.time.Duration
+
+class HoldRoomScenario(
+    private val maxConcurrentHolds: Int,
+    private val roomHoldDuration: Duration,
+    private val clock: Clock,
+    private val roomHoldRepository: RoomHoldRepository,
+) : HoldRoomPort {
+
+    override fun run(details: HoldRoomDetail): HoldRoomResult {
+        val holdToRemove = pickHoldToRemove(details.userId, details.roomTypeId)
+
+        val roomHoldExpiry = clock.instant().plus(roomHoldDuration)
+        val roomHoldResult = roomHoldRepository.createRoomHold(
+            userId = details.userId,
+            roomTypeId = details.roomTypeId,
+            roomHoldExpiry = roomHoldExpiry,
+            holdStartDate = details.holdStartDate,
+            holdEndDate = details.holdEndDate,
+            holdIdToRemove = holdToRemove,
+        )
+
+        return when (roomHoldResult) {
+            is CreateRoomHoldResult.RoomHoldCreated -> HoldRoomResult.RoomHoldCreated(
+                roomHoldId = roomHoldResult.roomHoldId,
+                holdExpiry = roomHoldExpiry,
+                removedRoomHoldId = holdToRemove,
+            )
+
+            is CreateRoomHoldResult.StockNotAvailable -> HoldRoomResult.StockNotAvailable
+        }
+    }
+
+    private fun pickHoldToRemove(userId: String, roomTypeId: String): String? {
+        val existingHolds = roomHoldRepository.findHoldsForUser(userId)
+
+        val holdOnSameRoom = existingHolds.firstOrNull { it.roomTypeId == roomTypeId }
+            ?.roomHoldId
+        if (holdOnSameRoom != null) {
+            return holdOnSameRoom
+        }
+
+        return if (existingHolds.size >= maxConcurrentHolds) {
+            existingHolds.minBy { it.holdExpiry }.roomHoldId
+        } else {
+            null
+        }
+    }
+}
--- be/domain/src/test/kotlin/me/aburke/hotelbooking/scenario/room/HoldRoomScenarioTest.kt
+++ be/domain/src/test/kotlin/me/aburke/hotelbooking/scenario/room/HoldRoomScenarioTest.kt
@@ -1,28 +1,277 @@
 package me.aburke.hotelbooking.scenario.room
 
-import org.junit.jupiter.api.Disabled
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import me.aburke.hotelbooking.ports.repository.CreateRoomHoldResult
+import me.aburke.hotelbooking.ports.repository.RoomHold
+import me.aburke.hotelbooking.ports.repository.RoomHoldRepository
+import me.aburke.hotelbooking.ports.scenario.room.HoldRoomDetail
+import me.aburke.hotelbooking.ports.scenario.room.HoldRoomResult
+import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.fail
+import org.junit.jupiter.api.extension.ExtendWith
+import java.time.Clock
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
-@Disabled
+private const val MAX_CONCURRENT_HOLDS = 3
+private const val USER_ID = "user-id"
+private const val ROOM_TYPE_ID = "room-type-id"
+private const val ROOM_HOLD_ID = "room-hold-id"
+private const val EXISTING_HOLD_ID1 = "existing-hold-id-1"
+private const val EXISTING_HOLD_ID2 = "existing-hold-id-2"
+private const val EXISTING_HOLD_ID3 = "existing-hold-id-3"
+private const val ALTERNATE_ROOM_TYPE_ID1 = "alternate-room-type-id-1"
+private const val ALTERNATE_ROOM_TYPE_ID2 = "alternate-room-type-id-2"
+private const val ALTERNATE_ROOM_TYPE_ID3 = "alternate-room-type-id-3"
+
+private val holdStartDate = LocalDate.parse("2023-05-10")
+private val holdEndDate = LocalDate.parse("2023-05-15")
+
+private val instant = Instant.now()
+private val roomHoldDuration = Duration.parse("PT30M")
+private val expectedExpiry = instant.plus(roomHoldDuration)
+private val existingHoldExpiry1 = instant.plus(2, ChronoUnit.MINUTES)
+private val existingHoldExpiry2 = instant.plus(3, ChronoUnit.MINUTES)
+private val existingHoldExpiry3 = instant.plus(4, ChronoUnit.MINUTES)
+
+@ExtendWith(MockKExtension::class)
 class HoldRoomScenarioTest {
 
     // TODO acquire distributed lock on user ID to ensure that they can't exceed their hold quota by creating race
     // conditions via concurrently calling hold endpoint
 
+    @MockK
+    lateinit var clock: Clock
+
+    @MockK
+    lateinit var roomHoldRepository: RoomHoldRepository
+
+    private lateinit var underTest: HoldRoomScenario
+
+    @BeforeEach
+    fun init() {
+        underTest = HoldRoomScenario(
+            maxConcurrentHolds = MAX_CONCURRENT_HOLDS,
+            roomHoldDuration = roomHoldDuration,
+            clock = clock,
+            roomHoldRepository = roomHoldRepository,
+        )
+    }
+
     @Test
     fun `should create hold on room for user`() {
-        fail("TODO")
+        every { clock.instant() } returns instant
+        every {
+            roomHoldRepository.findHoldsForUser(USER_ID)
+        } returns emptyList()
+        every {
+            roomHoldRepository.createRoomHold(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                roomHoldExpiry = expectedExpiry,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+                holdIdToRemove = null,
+            )
+        } returns CreateRoomHoldResult.RoomHoldCreated(ROOM_HOLD_ID)
+
+        val result = underTest.run(
+            HoldRoomDetail(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+            ),
+        )
+
+        assertSoftly { s ->
+            s.assertThat(result).isEqualTo(
+                HoldRoomResult.RoomHoldCreated(
+                    roomHoldId = ROOM_HOLD_ID,
+                    holdExpiry = expectedExpiry,
+                    removedRoomHoldId = null,
+                ),
+            )
+            s.check {
+                verify(exactly = 1) {
+                    clock.instant()
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.findHoldsForUser(USER_ID)
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.createRoomHold(
+                        userId = USER_ID,
+                        roomTypeId = ROOM_TYPE_ID,
+                        roomHoldExpiry = expectedExpiry,
+                        holdStartDate = holdStartDate,
+                        holdEndDate = holdEndDate,
+                        holdIdToRemove = null,
+                    )
+                }
+            }
+            s.check {
+                confirmVerified(
+                    clock,
+                    roomHoldRepository,
+                )
+            }
+        }
     }
 
     @Test
     fun `should replace an existing hold on the same room`() {
-        fail("TODO")
+        every { clock.instant() } returns instant
+        every {
+            roomHoldRepository.findHoldsForUser(USER_ID)
+        } returns listOf(
+            RoomHold(
+                roomHoldId = EXISTING_HOLD_ID1,
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                holdExpiry = existingHoldExpiry1,
+            ),
+        )
+        every {
+            roomHoldRepository.createRoomHold(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                roomHoldExpiry = expectedExpiry,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+                holdIdToRemove = EXISTING_HOLD_ID1,
+            )
+        } returns CreateRoomHoldResult.RoomHoldCreated(ROOM_HOLD_ID)
+
+        val result = underTest.run(
+            HoldRoomDetail(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+            ),
+        )
+
+        assertSoftly { s ->
+            s.assertThat(result).isEqualTo(
+                HoldRoomResult.RoomHoldCreated(
+                    roomHoldId = ROOM_HOLD_ID,
+                    holdExpiry = expectedExpiry,
+                    removedRoomHoldId = EXISTING_HOLD_ID1,
+                ),
+            )
+            s.check {
+                verify(exactly = 1) {
+                    clock.instant()
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.findHoldsForUser(USER_ID)
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.createRoomHold(
+                        userId = USER_ID,
+                        roomTypeId = ROOM_TYPE_ID,
+                        roomHoldExpiry = expectedExpiry,
+                        holdStartDate = holdStartDate,
+                        holdEndDate = holdEndDate,
+                        holdIdToRemove = EXISTING_HOLD_ID1,
+                    )
+                }
+            }
+            s.check {
+                confirmVerified(
+                    clock,
+                    roomHoldRepository,
+                )
+            }
+        }
     }
 
     @Test
     fun `should allow multiple holds to be created provided they are on different rooms`() {
-        fail("TODO")
+        every { clock.instant() } returns instant
+        every {
+            roomHoldRepository.findHoldsForUser(USER_ID)
+        } returns listOf(
+            RoomHold(
+                roomHoldId = EXISTING_HOLD_ID1,
+                userId = USER_ID,
+                roomTypeId = ALTERNATE_ROOM_TYPE_ID1,
+                holdExpiry = existingHoldExpiry1,
+            ),
+        )
+        every {
+            roomHoldRepository.createRoomHold(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                roomHoldExpiry = expectedExpiry,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+                holdIdToRemove = null,
+            )
+        } returns CreateRoomHoldResult.RoomHoldCreated(ROOM_HOLD_ID)
+
+        val result = underTest.run(
+            HoldRoomDetail(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+            ),
+        )
+
+        assertSoftly { s ->
+            s.assertThat(result).isEqualTo(
+                HoldRoomResult.RoomHoldCreated(
+                    roomHoldId = ROOM_HOLD_ID,
+                    holdExpiry = expectedExpiry,
+                    removedRoomHoldId = null,
+                ),
+            )
+            s.check {
+                verify(exactly = 1) {
+                    clock.instant()
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.findHoldsForUser(USER_ID)
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.createRoomHold(
+                        userId = USER_ID,
+                        roomTypeId = ROOM_TYPE_ID,
+                        roomHoldExpiry = expectedExpiry,
+                        holdStartDate = holdStartDate,
+                        holdEndDate = holdEndDate,
+                        holdIdToRemove = null,
+                    )
+                }
+            }
+            s.check {
+                confirmVerified(
+                    clock,
+                    roomHoldRepository,
+                )
+            }
+        }
     }
 
     /**
@@ -33,11 +282,144 @@ class HoldRoomScenarioTest {
      */
     @Test
     fun `should limit the total number of holds for a user`() {
-        fail("TODO")
+        every { clock.instant() } returns instant
+        every {
+            roomHoldRepository.findHoldsForUser(USER_ID)
+        } returns listOf(
+            RoomHold(
+                roomHoldId = EXISTING_HOLD_ID1,
+                userId = USER_ID,
+                roomTypeId = ALTERNATE_ROOM_TYPE_ID1,
+                holdExpiry = existingHoldExpiry1,
+            ),
+            RoomHold(
+                roomHoldId = EXISTING_HOLD_ID2,
+                userId = USER_ID,
+                roomTypeId = ALTERNATE_ROOM_TYPE_ID2,
+                holdExpiry = existingHoldExpiry2,
+            ),
+            RoomHold(
+                roomHoldId = EXISTING_HOLD_ID3,
+                userId = USER_ID,
+                roomTypeId = ALTERNATE_ROOM_TYPE_ID3,
+                holdExpiry = existingHoldExpiry3,
+            ),
+        ).shuffled()
+        every {
+            roomHoldRepository.createRoomHold(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                roomHoldExpiry = expectedExpiry,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+                holdIdToRemove = EXISTING_HOLD_ID1,
+            )
+        } returns CreateRoomHoldResult.RoomHoldCreated(ROOM_HOLD_ID)
+
+        val result = underTest.run(
+            HoldRoomDetail(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+            ),
+        )
+
+        assertSoftly { s ->
+            s.assertThat(result).isEqualTo(
+                HoldRoomResult.RoomHoldCreated(
+                    roomHoldId = ROOM_HOLD_ID,
+                    holdExpiry = expectedExpiry,
+                    removedRoomHoldId = EXISTING_HOLD_ID1,
+                ),
+            )
+            s.check {
+                verify(exactly = 1) {
+                    clock.instant()
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.findHoldsForUser(USER_ID)
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.createRoomHold(
+                        userId = USER_ID,
+                        roomTypeId = ROOM_TYPE_ID,
+                        roomHoldExpiry = expectedExpiry,
+                        holdStartDate = holdStartDate,
+                        holdEndDate = holdEndDate,
+                        holdIdToRemove = EXISTING_HOLD_ID1,
+                    )
+                }
+            }
+            s.check {
+                confirmVerified(
+                    clock,
+                    roomHoldRepository,
+                )
+            }
+        }
     }
 
     @Test
     fun `should return StockNotAvailable when there is not enough available stock to create the hold`() {
-        fail("TODO")
+        every { clock.instant() } returns instant
+        every {
+            roomHoldRepository.findHoldsForUser(USER_ID)
+        } returns emptyList()
+        every {
+            roomHoldRepository.createRoomHold(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                roomHoldExpiry = expectedExpiry,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+                holdIdToRemove = null,
+            )
+        } returns CreateRoomHoldResult.StockNotAvailable
+
+        val result = underTest.run(
+            HoldRoomDetail(
+                userId = USER_ID,
+                roomTypeId = ROOM_TYPE_ID,
+                holdStartDate = holdStartDate,
+                holdEndDate = holdEndDate,
+            ),
+        )
+
+        assertSoftly { s ->
+            s.assertThat(result).isEqualTo(HoldRoomResult.StockNotAvailable)
+            s.check {
+                verify(exactly = 1) {
+                    clock.instant()
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.findHoldsForUser(USER_ID)
+                }
+            }
+            s.check {
+                verify(exactly = 1) {
+                    roomHoldRepository.createRoomHold(
+                        userId = USER_ID,
+                        roomTypeId = ROOM_TYPE_ID,
+                        roomHoldExpiry = expectedExpiry,
+                        holdStartDate = holdStartDate,
+                        holdEndDate = holdEndDate,
+                        holdIdToRemove = null,
+                    )
+                }
+            }
+            s.check {
+                confirmVerified(
+                    clock,
+                    roomHoldRepository,
+                )
+            }
+        }
     }
 }
